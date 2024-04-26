4b5fab6a3e40251824107a2a65903991b31da853
Aurélien Le Hyaric <aurelien.lehyaric@live.com>
2024-04-26 16:53:44 +0200

    Connexion mongodb

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://aurelien:JSlJKalWSJcNiewM@atlascluster.hdy3j8v.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use(express.json());
 
-app.post((req, res, next) => {
+
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.post('/api/book', (req, res, next) => {
+app.post('/api/books', (req, res, next) => {
   console.log(req.body);
   res.status(201).json({
-    message: 'votre requête a bien été envoyé !'
+    message: 'test de livre créé !'
   });
 });
 
-
-app.get('/api/book', (req, res, next) => {
-  res.status(200).json({ message: 'Votre requête a bien été reçue !' }); 
+app.get('/api/books', (req, res, next) => {
+  const books = [
+    {
+      id: 1,
+      title: "Nom du livre"
+    }
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
